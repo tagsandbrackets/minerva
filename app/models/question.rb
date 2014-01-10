9f7653fbed77ce343abc7f580967401cc9cd566c
orodriguez <omarjavier15@gmail.com>
2014-01-10 16:43:05 -0400

    Question::recent wraps convert AR objects into Entities::Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,10 @@
 class Question < ActiveRecord::Base
   def self.recent
-    order(:created_at).limit(10)
+    order(:created_at).limit(10).map &:to_entity
+  end
+
+  def to_entity
+    Minerva::Entities::Question.new title: self.title, 
+      created_at: self.created_at
   end
 end
